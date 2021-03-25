51305f3f15ab7fcf7ed855a435b3119fd141e198
Tyler Cheney <tyler.cheney.05@gmail.com>
2021-03-24 21:01:16 -0600

    created react app, model, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    resources :profiles do
+      resources :posts
+    end
+  end
+
+  # resources :posts do
+  #   resources :comments
+  # end
+
 end
